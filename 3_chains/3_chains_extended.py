403c15c9c66dc8bb44b6c20cca543ecaa87ffc80
Andy Liu <andyliu0330@gmail.com>
2024-06-24 11:26:04 -0700

    chore: Update chat models to use Ollama instead of ChatOpenAI

--- 3_chains/3_chains_extended.py
+++ 3_chains/3_chains_extended.py
@@ -2,13 +2,15 @@ from dotenv import load_dotenv
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.output_parser import StrOutputParser
 from langchain.schema.runnable import RunnableLambda
-from langchain_openai import ChatOpenAI
+# from langchain_openai import ChatOpenAI
+from langchain_community.llms import Ollama
 
 # Load environment variables from .env
-load_dotenv()
+# load_dotenv()
 
 # Create a ChatOpenAI model
-model = ChatOpenAI(model="gpt-4o")
+# model = ChatOpenAI(model="gpt-4o")
+model = Ollama(model="llama3:8b")
 
 # Define prompt templates
 prompt_template = ChatPromptTemplate.from_messages(
