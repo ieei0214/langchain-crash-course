7bf473cc008c03088c1676ee2a9a4b1c4f651746
Andy Liu <andyliu0330@gmail.com>
2024-06-24 10:32:49 -0700

    chore: Update chat models to use Ollama instead of ChatOpenAI

--- 2_prompt_templates/2_prompt_template_with_chat_model.py
+++ 2_prompt_templates/2_prompt_template_with_chat_model.py
@@ -1,12 +1,14 @@
 from dotenv import load_dotenv
 from langchain.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
+# from langchain_openai import ChatOpenAI
+from langchain_community.llms import Ollama
 
 # Load environment variables from .env
-load_dotenv()
+# load_dotenv()
 
 # Create a ChatOpenAI model
-model = ChatOpenAI(model="gpt-4o")
+# model = ChatOpenAI(model="gpt-4o")
+model = Ollama(model="llama3:8b")
 
 # PART 1: Create a ChatPromptTemplate using a template string
 print("-----Prompt from Template-----")
@@ -15,7 +17,8 @@ prompt_template = ChatPromptTemplate.from_template(template)
 
 prompt = prompt_template.invoke({"topic": "cats"})
 result = model.invoke(prompt)
-print(result.content)
+# print(result.content)
+print(result)
 
 # PART 2: Prompt with Multiple Placeholders
 print("\n----- Prompt with Multiple Placeholders -----\n")
@@ -26,7 +29,8 @@ prompt_multiple = ChatPromptTemplate.from_template(template_multiple)
 prompt = prompt_multiple.invoke({"adjective": "funny", "animal": "panda"})
 
 result = model.invoke(prompt)
-print(result.content)
+# print(result.content)
+print(result)
 
 # PART 3: Prompt with System and Human Messages (Using Tuples)
 print("\n----- Prompt with System and Human Messages (Tuple) -----\n")
@@ -37,4 +41,5 @@ messages = [
 prompt_template = ChatPromptTemplate.from_messages(messages)
 prompt = prompt_template.invoke({"topic": "lawyers", "joke_count": 3})
 result = model.invoke(prompt)
-print(result.content)
+# print(result.content)
+print(result)
